309ea8627e4b3dfd11ca6d1bf5b6167f8f89ce9b
Alejandro Cortes Cortez <alejandro.cortes.c@usach.cl>
2022-04-29 00:13:42 -0400

    adaptando el algoritmo de division

--- LAB_2/division_iterativa.py
+++ LAB_2/division_iterativa.py
@@ -3,14 +3,11 @@ def div(i, n):
     while i >= n:
         i = i - n
         acum += 1
-    rep = 0
     i = i * 100
     resto = 0
-    while rep < 2: # Hay resto
-        rep += 1
-        while i >= n:
-            i = i - n
-            resto += 1
+    while i >= n:
+        i = i - n
+        resto += 1
     return acum , resto
 
-print(div(1,2))
+print(div(5,2))
