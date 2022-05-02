e47190f7f62080153b3cdd6c7db2cbf402d8ae04
LilyisusUSACH <alejandro.cortes.c@usach.cl>
2022-05-02 15:33:17 -0400

    Mejora en orden y formato parte3

--- LAB_2/collatz.asm
+++ LAB_2/collatz.asm
@@ -1,46 +1,72 @@
 .data
-	newline : .byte '\n'
+	entrada: .asciiz "Ingrese un numero inicial : "
+	textConverge : .asciiz "La Conjetura de collatz converge \n"
+	textNoConverge : .asciiz "Dentro de las limitaciones, esta secuencia NO converge \n"
+	
 .text
-
-li $t0, 715827882 #limite de reg 2147483647
-# tiene que ser < 715.827.882
-li $t1, 0x100100A0
-# lim memoria 348 o 0x10010200
-li $t2, 0x10040000
-
-addi $s0,$zero, 77031 # numero de secuencia, pedirlo por consola
-
-addi $s1,$zero,2 # para tener un dos al dividir
-
-mientras:
-	sw $s0, 0($t1)
-	addi $t1,$t1,4
+main:
+	jal leer_entradas
+	add $a0,$zero,$v0
+	jal secuencia_collatz
+	j exit
 	
-	beq $t1, $t2, fin
-	beq $s0, 1, converge
-	
-	div $s0,$s1
-	mfhi $t3 # remainder
-	
-	beqz $t3, par # Es par
-	
-	impar:
-		bgt $s0, $t0, fin
-		mul $s0,$s0,3
-		addi $s0, $s0,1
-		j mientras
-	par:
-		div $s0,$s0,$s1
-		j mientras
-	converge:
-		add $a0, $zero, $s0
-		li $v0, 1
-		syscall
-		j fin
-
-fin:
-	#salida
-	#beq $s0,1, 
-	
+exit: 
 	li $v0,10
 	syscall
+	
+leer_entradas:
+	la $a0, entrada
+	li $v0, 4
+	syscall
+	li $v0, 5
+	syscall
+	jr $ra
+
+secuencia_collatz:
+	li $t0, 715827882 #limite de reg 2147483647
+	# tiene que ser < 715.827.882
+	li $t1, 0x100100A0
+	# lim memoria 348 o 0x10010200
+	li $t2, 0x10040000
+
+	add $s0,$zero,$a0 # numero de secuencia, pedirlo por consola
+
+	addi $s1,$zero,2 # para tener un dos al dividir
+
+	mientras:
+		sw $s0, 0($t1)
+		addi $t1,$t1,4
+	
+		beq $t1, $t2, noConverge
+		beq $s0, 1, converge
+		
+		div $s0,$s1
+		mfhi $t3 # remainder
+	
+		beqz $t3, par # Es par
+	
+		impar:
+			bgt $s0, $t0, noConverge
+			mul $s0,$s0,3
+			addi $s0, $s0,1
+			j mientras
+		par:
+			div $s0,$s0,$s1
+			j mientras
+		converge:
+			la $a0,textConverge
+			li $v0, 4
+			syscall
+			j fin
+		noConverge:
+			la $a0,textNoConverge
+			li $v0, 4
+			syscall
+			j fin
+
+	fin:
+		add $t0,$zero,$zero
+		add $t1,$zero,$zero
+		add $t2,$zero,$zero
+		move $v0, $s0
+		jr $ra
